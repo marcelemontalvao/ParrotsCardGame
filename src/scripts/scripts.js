4d196cb1c6c88b1779a0cfc4da5e1d9648b096f3
marcelemontalvao <mmontalvaoblog@outlook.com>
2022-10-24 08:17:20 -0300

    Bug flip card

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -53,3 +53,101 @@ function createCards(imgFront) {
     `
     return li;
 }
+/*
+function turnCard(element) {
+    console.log(element);
+    let arrayCardsTurned = [];
+    const backface = element.parentNode.querySelector(".back-face");
+    const frontface = element.parentNode.querySelector(".front-face");
+    
+    if(frontface.classList.contains("hidden")) {
+        backface.classList.add("hidden");
+        backface.classList.remove("face");
+        frontface.classList.remove("hidden");
+        frontface.classList.add("face"); 
+        arrayCardsTurned.push(backface);
+    } else {
+        backface.classList.add("face");
+        backface.classList.remove("hidden");
+        frontface.classList.remove("face");
+        frontface.classList.add("hidden"); 
+    }
+}
+
+function handleCards(card1, card2) {
+    console.log(card1.id);
+    console.log(card2.id);
+    if(card1.id != card2.id) {  
+        console.log("CAIU AQUI")
+        setInterval(() => {
+            turnCard(card1);
+            turnCard(card2);
+        }, 1000);      
+    } else {
+        console.log("caiu aqui")
+        /*if(!arrayCardsTurned.find(card1)) {
+            arrayCardsTurned.push(card1, card2);
+        }
+        console.log("ARRAY: ", arrayCardsTurned);
+    }
+}
+*/
+function cardClick() {
+    const cards = document.querySelectorAll(".card");
+    let firstCard = "";
+    let secondCard = "";
+    cards.forEach(card => {
+        card.addEventListener("click", (event)=> {
+
+            if(firstCard == undefined) {
+                event.target.classList.add("hidden");
+                const front = event.target.parentNode.querySelector(".front-face")
+                front.classList.remove("hidden");
+                firstCard = card;
+
+            } else { 
+                event.target.classList.add("hidden");
+                const front = event.target.parentNode.querySelector(".front-face")
+                front.classList.remove("hidden");
+                secondCard = card;
+                if(firstCard.id != secondCard.id) {  
+                    setTimeout(() => {
+                    const front1 = firstCard.querySelector(".front-face")
+                    front1.classList.add("hidden");
+                    const back1 = firstCard.querySelector(".back-face")
+                    back1.classList.remove('hidden');
+                    
+                    const front2 = secondCard.querySelector(".front-face")
+                    front2.classList.add("hidden");
+                    const back2 = secondCard.querySelector(".back-face")
+                    back2.classList.remove('hidden');
+                    }, 1000);
+                    firstCard = undefined;
+                    secondCard = undefined;      
+                } else {
+                    console.log("caiu aqui") 
+                    firstCard = undefined;
+                    secondCard = undefined; 
+                }
+            }
+            
+            plays++;
+            //endGame();
+        })
+    })
+}
+
+
+cardsDistribution();
+
+
+/*
+function endGame() {
+   // const back = document.querySelectorAll(".back-face.hidden,")
+    const message = `
+    Você ganhou em ${plays} jogadas!
+    `
+    if(back.length == 0) {
+        alert(message);
+    }
+}*/
